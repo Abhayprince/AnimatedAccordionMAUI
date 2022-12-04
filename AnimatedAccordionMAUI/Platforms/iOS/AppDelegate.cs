4feb6f8119693077b1ec4d258dd50fcebc4d1b47
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2022-12-04 22:38:01 +0530

    Add project files.

---
+++ AnimatedAccordionMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AnimatedAccordionMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
